dd24e2e56e7006a38f2e50eb2eaf77ee10c6f902
ssumika <gsuzuki221@gmail.com>
2022-04-15 01:26:30 +0900

    timerをmasterにmerge

--- product/src/components/page_2.vue
+++ product/src/components/page_2.vue
@@ -1,8 +1,30 @@
 <template>
-   <p>ここは2ページです。</p>
+   <div id='app'>
+      <v-calendar :attributes="attrs" ></v-calendar>         
+   </div>
 </template>
 
 <script>
+
+export default({
+  name: 'CalendarTest',
+  data(){
+     return{
+      attrs: [
+         {
+            key: 'today',
+            highlight: {
+               backgroundColor: '#ff8080',
+            },
+            dates: new Date(),
+            popover: {
+               label: 'メッセージを表示できます',
+            },
+         }
+      ],
+   }
+  }
+})
 </script>
 
 <style>
